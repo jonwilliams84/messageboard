13db740ac62dcd0c5894676eec6ce385f006a2a5
Jon Williams <jonathan.williams@capgemini.com>
2020-06-17 11:19:03 +0100

    updated ADMIN_URL to use fqdn

--- src/Constants.js
+++ src/Constants.js
@@ -1,2 +1,2 @@
 export const URL = "http://localhost:42069/settings"
-export const ADMIN_URL = "http://10.32.10.194:42069/settings"
+export const ADMIN_URL = "http://msgboardserver.sifragalot.com:42069/settings"
