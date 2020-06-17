902cfeaa937fe70c660ecc0688958bdaa5810217
Jon Williams <jonathan.williams@capgemini.com>
2020-06-17 11:59:28 +0100

    fixed admin url

--- src/Constants.js
+++ src/Constants.js
@@ -1,2 +1,2 @@
 export const URL = "http://localhost:42069/settings"
-export const ADMIN_URL = "http://msgboardserver.sifragalot.com:42069/settings"
+export const ADMIN_URL = "http://msgboardserver.sirfragalot.com:42069/settings"
