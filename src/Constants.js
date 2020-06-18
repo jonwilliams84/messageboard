8a1bb659c89e2260e039bb64b0e002f62bb31e5b
Scott Janes <scott.janes@capgemini.com>
2020-06-18 11:53:51 +0100

    reverted constants

--- src/Constants.js
+++ src/Constants.js
@@ -1,3 +1,2 @@
 export const URL = "http://localhost:42069/settings"
-export const ADMIN_URL = "http://localhost:42069/settings"
-// export const ADMIN_URL = "http://msgboardserver.sirfragalot.com:42069/settings"
+export const ADMIN_URL = "http://msgboardserver.sirfragalot.com:42069/settings"
