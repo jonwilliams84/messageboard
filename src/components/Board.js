135584289d70d42c25cf0a3af5fa8cb2d51273ab
Jon Williams <jonathan.williams@capgemini.com>
2020-06-18 16:18:28 +0100

    line colours

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,19 +3,19 @@ import Stack from "../styles/Stack";
 import TextContainer from "../styles/TextContainer";
 import TextInstance from "../styles/TextInstance";
 
-const Board = ({formValues, backgroundColour}) => {
+const Board = ({formValues, backgroundColour, line1Colour, line2Colour, line3Colour }) => {
   console.log(backgroundColour)
-
+  
   return (
-    <Stack backgroundColour={backgroundColour}>
+    <Stack>
       <TextContainer>
-        <TextInstance>{formValues ? formValues.line1 : ""}</TextInstance>
+        <TextInstance line1Colour={formValues.line1Colour ? formValues.line1Colour : backgroundColour}>{formValues ? formValues.line1 : "" }</TextInstance>
       </TextContainer>
       <TextContainer>
-        <TextInstance>{formValues ? formValues.line2 : ""}</TextInstance>
+        <TextInstance line2Colour={formValues.line1Colour ? formValues.line2Colour : backgroundColour}>{formValues ? formValues.line2 : ""}</TextInstance>
       </TextContainer>
       <TextContainer>
-        <TextInstance>{formValues ? formValues.line3 : ""}</TextInstance>
+        <TextInstance line3Colour={formValues.line1Colour ? formValues.line3Colour : backgroundColour}>{formValues ? formValues.line3 : ""}</TextInstance>
       </TextContainer>
     </Stack>
   );
